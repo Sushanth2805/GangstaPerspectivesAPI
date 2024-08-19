439c6ff9961c31f31c23de2dd8ad311f90b787e7
Sushanth2805 <sushanth.2805@gmail.com>
2024-08-19 19:03:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 import requests
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-YOUTUBE_API_KEY = 'AIzaSyD3YtxM00R8LeGC0YXmrRyh4TEw2qc9EeQ'  # Replace with your actual API key
-CHANNEL_ID = 'UCNTdQYhTtb13-IYKYCW0d1A'  # Your provided channel ID
+# Access the API key from the environment variable
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+CHANNEL_ID = 'UCNTdQYhTtb13-IYKYCW0d1A'
 
 @app.route('/')
 def home():
@@ -12,7 +18,7 @@ def home():
 
 @app.route('/latest_videos')
 def latest_videos():
-    url = f'https://www.googleapis.com/youtube/v3/search?key={YOUTUBE_API_KEY}&channelId={CHANNEL_ID}&part=snippet,id&order=date&maxResults=20'
+    url = f'https://www.googleapis.com/youtube/v3/search?key={YOUTUBE_API_KEY}&channelId={CHANNEL_ID}&part=snippet,id&order=date&maxResults=10'
     response = requests.get(url)
     data = response.json()
     videos = []
@@ -20,7 +26,7 @@ def latest_videos():
         video_info = {
             'title': item['snippet']['title'],
             'videoId': item['id']['videoId'],
-            'thumbnail': item['snippet']['thumbnails']['medium']['url'],  # Get the medium size thumbnail
+            'thumbnail': item['snippet']['thumbnails']['medium']['url'],
             'published_at': item['snippet']['publishedAt']
         }
         videos.append(video_info)
